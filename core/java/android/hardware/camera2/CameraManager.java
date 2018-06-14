c94352d4118a7b9a1bcb4e5dd556e29b1013f228
th12289 <tim.hallinger@googlemail.com>
2018-06-14 16:00:55 +0200

    FIX WIDESCREEN GCAM
    Revert "Camera: Expose Aux camera to apps present in the whitelist"
    
    This reverts commit d0b8038550893c9f5f9278724799be2726358eb5.

--- core/java/android/hardware/camera2/CameraManager.java
+++ core/java/android/hardware/camera2/CameraManager.java
@@ -21,7 +21,6 @@ import android.annotation.Nullable;
 import android.annotation.RequiresPermission;
 import android.annotation.SystemService;
 import android.content.Context;
-import android.hardware.Camera;
 import android.hardware.CameraInfo;
 import android.hardware.CameraStatus;
 import android.hardware.ICameraService;
@@ -842,7 +841,6 @@ public final class CameraManager {
 
                 int idCount = 0;
                 for (int i = 0; i < mDeviceStatus.size(); i++) {
-                    if ((i == 2) && !Camera.shouldExposeAuxCamera()) break;
                     int status = mDeviceStatus.valueAt(i);
                     if (status == ICameraServiceListener.STATUS_NOT_PRESENT ||
                             status == ICameraServiceListener.STATUS_ENUMERATING) continue;
@@ -851,7 +849,6 @@ public final class CameraManager {
                 cameraIds = new String[idCount];
                 idCount = 0;
                 for (int i = 0; i < mDeviceStatus.size(); i++) {
-                    if ((i == 2) && !Camera.shouldExposeAuxCamera()) break;
                     int status = mDeviceStatus.valueAt(i);
                     if (status == ICameraServiceListener.STATUS_NOT_PRESENT ||
                             status == ICameraServiceListener.STATUS_ENUMERATING) continue;
@@ -988,16 +985,6 @@ public final class CameraManager {
         }
 
         private void onStatusChangedLocked(int status, String id) {
-            /* Force to ignore the last mono/aux camera status update
-             * if the package name does not falls in this bucket
-             */
-            if (!Camera.shouldExposeAuxCamera()) {
-                if (Integer.parseInt(id) >= 2) {
-                    Log.w(TAG, "[soar.cts] ignore the status update of camera: " + id);
-                    return;
-                }
-            }
-
             if (DEBUG) {
                 Log.v(TAG,
                         String.format("Camera id %s has status changed to 0x%x", id, status));
